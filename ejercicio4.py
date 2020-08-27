2b8ede3413cbac06b3f2061d83cb9937bb1d3fa1
Randy Campuzano <rjcampuzano@uniguajira.edu.co>
2020-08-26 23:13:05 -0500

    no di para hacerlo

---
+++ ejercicio4.py
@@ -0,0 +1,12 @@
+import math
+
+math.e ** 2  # 7.3890560989306495
+math.exp(2)  # 7.38905609893065
+
+import cmath
+cmath.e ** 2 # 7.3890560989306495
+cmath.exp(2) # (7.38905609893065+0j)
+print(math.e ** 5)       # 22026.465794806703
+print(math.exp(10))       # 22026.465794806718
+print(cmath.exp(10).real) # 22026.465794806718
+#     difference starts here ---------------^
